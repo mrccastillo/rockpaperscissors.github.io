983282856997b2f03e2d4ba904d978d72ced935d
mrccastillo <124692706+mrccastillo@users.noreply.github.com>
2023-03-15 01:50:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,72 @@
-//use prompt to know the name of the player and store it in a variable
+//score counter
+let playerScore = 0;
+let computerScore = 0;
+let gameRounds = 0;
+//initialization of names and rounds
+const playBtn = document.getElementById('play-game-btn');
+const rounds = document.querySelector('#rounds');
+playBtn.addEventListener('click', function () {
+    if (!rounds.value) {
+        alert('Please input how many rounds you are going to play');
+    }
+    else {
+        document.querySelector('.initial-modal').classList.add('hidden');
+        document.querySelector('.overlay').classList.add('hidden');
 
-// let pName = prompt('Please enter your Name to play the game');
-// if (!pName) {
-//     document.querySelector('#player-name').textContent = 'Player 1';
-// }
-// else {
-//     document.querySelector('#player-name').textContent = pName;
-// }
-//will use it later
-
+    }
+    gameRounds += Number(rounds.value);
+});
 
 //powerful and most advanced ai for rock paper scissor
+const pScore = document.getElementById('p-score');
+const compScore = document.getElementById('comp-score');
+
 const compPickImage = document.getElementById('comp-pick-img');
+const gameRes = document.querySelector('.game-results');
+
 let results = function computerPick() {
     let rand = Math.floor(Math.random() * 3) + 1;
-    compPickImage.src = `images/item-${rand}.jpg`;
+    compPickImage.src = `images/item-${rand}.png`;
 
     //rock
     if (rand === 3 && playerPick === 1) {
-        document.querySelector('#game-results-display').innerHTML = 'Player Won';
+        document.querySelector('#game-results-display').textContent = 'Player Won';
+        playerScore += 1;
         // return 'player won';
     }
     else if (rand === 2 && playerPick === 1) {
-        document.querySelector('#game-results-display').innerHTML = 'Player Lost';
+        document.querySelector('#game-results-display').textContent = 'Player Lost';
+        computerScore += 1;
         // return 'player lost';
     }
     //paper
     else if (rand === 1 && playerPick === 2) {
-        document.querySelector('#game-results-display').innerHTML = 'Player Won';
+        document.querySelector('#game-results-display').textContent = 'Player Won';
+        playerScore += 1;
         // return 'player won';
     }
     else if (rand === 3 && playerPick === 2) {
-        document.querySelector('#game-results-display').innerHTML = 'Player Lost';
+        document.querySelector('#game-results-display').textContent = 'Player Lost';
+        computerScore += 1;
         // return 'player lost';
     }
     //scissors
     else if (rand === 2 && playerPick === 3) {
-        document.querySelector('#game-results-display').innerHTML = 'Player Won';
+        document.querySelector('#game-results-display').textContent = 'Player Won';
+        playerScore += 1;
         // return 'player won';
     }
     else if (rand === 1 && playerPick === 3) {
-        document.querySelector('#game-results-display').innerHTML = 'Player Lost';
+        document.querySelector('#game-results-display').textContent = 'Player Lost';
+        computerScore += 1;
         // return 'player lost';
     }
     else {
-        document.querySelector('#game-results-display').innerHTML = 'Draw';
+        document.querySelector('#game-results-display').textContent = 'Draw';
     }
+    pScore.textContent = playerScore;
+    compScore.textContent = computerScore;
+    gameRes.classList.remove('hidden');
 }
 
 //for the player pick
@@ -55,20 +76,29 @@ const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
 let playerPick;
 
+let roundCount = 0;
+
+
 rockBtn.addEventListener('click', function () {
-    playerPickImage.src = 'images/item-1.jpg'
+    playerPickImage.src = 'images/item-1.png'
     playerPick = 1;
     results();
+    roundCount++;
+    console.log(roundCount);
 })
 
 paperBtn.addEventListener('click', function () {
-    playerPickImage.src = 'images/item-2.jpg'
+    playerPickImage.src = 'images/item-2.png'
     playerPick = 2;
     results();
+    roundCount++;
+    console.log(roundCount);
 })
 
 scissorsBtn.addEventListener('click', function () {
-    playerPickImage.src = 'images/item-3.jpg'
+    playerPickImage.src = 'images/item-3.png'
     playerPick = 3;
     results();
+    roundCount++;
+    console.log(roundCount);
 })
